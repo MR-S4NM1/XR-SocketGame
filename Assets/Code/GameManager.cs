fd3ef56317f96345e58b9765ac5be5b6f375f972
MR-S4NM1 <miguelitogarcia30@outlook.com>
2025-03-11 20:39:14 -0600

    UI and Game Manager_03-11-2025

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region References
+    [SerializeField] protected XRGrabInteractable _doorXRGrabInteractable;
+    [SerializeField] protected GameObject _treasureGO;
+    [SerializeField] protected ParticleSystem[] _explosionsSystem;
+    #endregion
+
+    #region Runtime Variables
+    [SerializeField] protected short _explosionTimer;
+    [SerializeField] protected bool _playerHasKey;
+    protected Coroutine _trapCoroutine;
+    #endregion
+
+    #region Unity Methods
     void Start()
     {
-        
+        _playerHasKey = false;
+        _doorXRGrabInteractable.enabled = _playerHasKey;
+        _treasureGO.SetActive(false);
+    }
+    #endregion
+
+    #region Public Methods
+
+    public void ActivateDoorOpening()
+    {
+        _playerHasKey = true;
+        _doorXRGrabInteractable.enabled = _playerHasKey;
+        _treasureGO.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DeactivateGoldAndActivateTrap()
     {
-        
+        _trapCoroutine = StartCoroutine(trapCoroutine());
     }
+    #endregion
+
+    #region Coroutines
+
+    protected IEnumerator trapCoroutine()
+    {
+        _explosionTimer = 10;
+        _treasureGO.SetActive(false);
+
+        while(_explosionTimer > 0)
+        {
+            _explosionTimer--;
+            yield return new WaitForSeconds(1.0f);
+        }
+
+        foreach(ParticleSystem particleSystem in _explosionsSystem)
+        {
+            particleSystem.Play();
+        }
+    }
+
+    #endregion
+
+    #region Getters and Setters
+    public bool SetPlayerHasKey
+    {
+        set { _playerHasKey = value; }
+    }
+    #endregion
 }
